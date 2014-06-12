7d924e6bb24faa81d32edc4e55ace536150dbbe9
Katie Hoffman <ktahoffman@gmail.com>
2014-06-12 15:59:31 -0400

    simplifies count_element method

--- collections_practice.rb
+++ collections_practice.rb
@@ -57,16 +57,15 @@ def find_cool(array)
 end
 
 def count_elements(array)
-  results = Hash.new(0)
-  array.each do |element|
-    results[element] += 1
-  end
-  container = []
-  results.each do |key, value|
-    key[:count] = value
-    container << key
-  end
-  container
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
 end
 
 def merge_data(keys, values)
