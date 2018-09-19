1ef932771c948eafeca52e8e531e8b390536a4bc
Manny Pissanos <mpissano@gmail.com>
2018-09-19 15:49:33 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all? do |word|
+    word[0] =="r"
+ end
+end
+
+def contain_a(arr)
+  arr.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(arr)	
+arr.find do |word|
+  word[0..1] == "wa"
+  end
+end
+
+def remove_non_strings(arr)
+  arr.delete_if { |obj| !(obj.is_a? String) }
+end
+
+def count_elements(array)
+  arr.
