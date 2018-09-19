84916c43a8d0e8b2f1fb333e925c559197438f46
Manny Pissanos <mpissano@gmail.com>
2018-09-19 22:19:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,6 +20,52 @@ def remove_non_strings(arr)
   arr.delete_if { |obj| !(obj.is_a? String) }
 end
 
-def count_elements(hash)
+def count_elements(arr)
+ output_arr = [];
+  arr.each do | element |
+    counter = element[:count] = 1
+    if output_arr.include?(element) == false
+      output_arr << element
+  elsif output_arr.include?(element)
+      output_arr.map do |el|
+        el[:count] += 1
+      end
+    end
+  end
+  output_arr
+end
   
-  hash.each do { key, val|
+
+def merge_data(keys, data)
+  merged = []
+  keys.each {|i| data.first.map {|k,v| if i.values[0] == k  then merged << i.merge(v) end}}
+  merged
+end
+
+def find_cool(cool)
+  i = 0
+  output = [];
+  while i < cool.length do
+    cool[i].collect do | key, values |
+      if values == "cool"
+        output << cool[i]
+      return output
+      end
+    end
+    i+=1
+  end
+end
+
+def organize_schools(schools)
+  output = {}
+  schools.each do |key, location_hash|
+    location_hash.each do |k, location|
+      if !output.has_key?(location)
+        output[location] = [key]
+      elsif output.has_key?(location)
+        output[location] << key
+      end
+    end
+  end
+  output
+end
