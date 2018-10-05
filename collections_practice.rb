b7d297f8f6238edc3a1e65ccc89234e71c1995e0
Brandon Teich <brandonteich123@gmail.com>
2018-10-05 04:51:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? do |i|
+   if i[0] != "r"
+     false
+   else
+     true
+  end
+end
+end
+
+def contain_a(array)
+  newarray = []
+  array.each do |i| 
+  if  i.include?("a")
+    newarray << i
+  end
+end
+newarray
+end
