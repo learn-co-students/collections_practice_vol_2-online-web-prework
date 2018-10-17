ee3e7d83bf4689705825c35214adfe31ca1a0795
VICTORIA MANUEL <victoriamanuel.dev@gmail.com>
2018-10-17 22:05:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
-# your code goes here
+def begins_with_r
+end
+
+def contain_a
+end
+
+def first_wa
+end
+
+def remove_non_strings
+end
+
+def count_elements
+end
+
+def merge_data
+end
+
+def find_cool
+end
+
+def organize_schools
+end
