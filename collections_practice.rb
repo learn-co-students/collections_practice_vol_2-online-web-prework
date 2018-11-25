3d796d5f6e83701d11b9e1d0f70973a2c17c31bb
Whitny Edwards <wne4e2@gmail.com>
2018-11-25 00:22:27 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
 # your code goes here
+def begins_with_r(array)
+  array.all? do |word|
+    word.downcase.chr == "r"
+  end
+end
+
+def contain_a (array)
+  array.select {|element| element.include?("a")}
+end
+
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.reject {|element| element.class != String}
+end
+
+
+def count_elements(array)
+  counts = Hash.new(0)
+  array.collect {|element| counts[element]+=1 }
+    counts.collect do |hash, number|
+      hash[:count] = number
+    end
+  counts.keys
+end
+
+def merge_data(keys, data)
+  keys.each do |name_hash|
+    data.each do |hash|
+      name_hash.merge!(hash[name_hash[:first_name]])
+    end
+  end
+end
+
+def find_cool(array)
+  array.select {|entry| entry if entry.has_value?("cool")}
+end
+
+def organize_schools(schools)
+  by_location = {}
+    schools.each do |school, location_hash|
+      location_hash.each do |symbol, location|
+        if by_location[location] == nil
+          by_location[location] = [school]
+        else
+          by_location[location] << school
+        end
+      end
+    end
+    by_location
+end
