25e25cd6bac39e841b84cdf953f840f59640abee
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-24 05:35:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
-# your code goes here
+require 'pry'
+def begins_with_r(array)
+
+array.include?('r')
+end
+def contain_a(array)
+array.keep_if {|v|  v.include?('a')}
+
+
+end
+def first_wa(array)
+end
