b8eaef98b07e4d04c78ab48bfcaaed11282b0314
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-24 22:32:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,12 @@
 require 'pry'
 def begins_with_r(array)
-
 array.include?('r')
+
 end
 def contain_a(array)
 array.keep_if {|v|  v.include?('a')}
-
-
 end
+
 def first_wa(arr)
   arr.select {|a| a.find(/^wa/)}
 end
