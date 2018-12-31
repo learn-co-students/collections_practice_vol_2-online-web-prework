380be240e4418128d5d76b2b78b7710695f18976
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-31 21:21:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def begins_with_r(array)
-array.include?('r')
+array.all?{|word|   word.include?('r')}
 
 end
 def contain_a(array)
@@ -8,5 +8,26 @@ array.keep_if {|v|  v.include?('a')}
 end
 
 def first_wa(arr)
-  arr.select {|a| a.find(/^wa/)}
+  arr.detect{ |a|    a.match(/wa/) }
+end
+def remove_non_strings(arr)
+arr.keep_if { |obj| (obj.is_a? String) }
+end
+
+def count_elements(arr)
+arr.group_by(&:itself)
+ .map{|k, v| k.merge(count: v.length) }
+
+end
+def merge_data(keys,data)
+keys.map do |name|    name.values[0]
+
+data.map do |info|  info.keys[0]
+info.values[0]
+if name.values[0]== info.keys[0]
+binding.pry
+name.merge(info.values[0])
+end
+end
+end
 end
