109ac74c45fce243609df6ed9f4b8866f4813f9f
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 04:49:48 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 # your code goes here
+def begins_with_r(array)
+  array.all?{|element| element[0] == 'r'}
+end
+
+def contain_a(array)
+  array.find_all{|element| element.include? "a"}
+end
+
+def first_wa(array)
+  array.find{|element| element[0..1] == 'wa'}
+end
+
+def remove_non_strings(array)
+  array.delete_if{|element| element.class != String}
+end
