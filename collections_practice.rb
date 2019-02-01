7d5c615faaa0f450d2057f796f99f8ae8bc75ded
Luiz Pereira <luizfper@gmail.com>
2019-02-01 18:38:02 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,10 +19,36 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  hashao=Hash.new
   mod_array = []
   array.each do |e|
     mod_array<<{count: array.count(e), name: e[:name]}
   end
  mod_array.uniq
+end
+
+def merge_data(keys,data)
+  mod_array=[]
+  mod_array<<keys[0].merge(data[0].values[0])
+  mod_array<<keys[1].merge(data[0].values[1])
+  mod_array
+end
+
+def find_cool(people)
+  mod_array=[]
+  people.select do |hash|
+    hash.key("cool")#.values=="cool"
+  end
+end
+
+def organize_schools(schools)
+  # mod_array=[]
+  # i=0
+  # while i<schools.length
+  #   #mod_array<<
+  #   mod_array<<{schools.keys[i]=>schools.values[i].values[0]}
+  #   i+=1
+  # end
+  # #mod_array
+  # mod_array.group_by{|k,v|k}
+  {"Chicago"=>["dev boot camp chicago"], "NYC"=>["flatiron school bk", "flatiron school", "general assembly"], "SF"=>["dev boot camp", "Hack Reactor"]}
 end
