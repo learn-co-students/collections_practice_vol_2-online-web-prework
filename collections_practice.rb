d3070963433516b0a723bd157b68b0e106347f5f
Christoph Gruener <christophgruener@me.com>
2019-02-04 21:50:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,12 +52,9 @@ def count_elements(array)
 end
 
 def merge_data(keys, data)
-  keys_combined = []
   names = []
   names.push(keys.values)
-  binding pry
-  keys_each do |name|
-    if data.include?(name[:first_name])
-      data[name.key]
+  names
 end
-    
+  
+  
