2cb5fb1d3e2eecb8c0170c3ac74411110663656a
Christoph Gruener <christophgruener@me.com>
2019-02-04 20:33:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,29 +1,22 @@
 def begins_with_r(array)
-  array.each do |word| 
-    if word(0) == "r" 
-       return true 
-      elsif word(0) != "r" 
-       return false 
+  word_with_r = true
+  array.each do |word|
+    if word.start_with?("r")
+       word_with_r = true
+     else
+       word_with_r = false
       end
     end
+    word_with_r
 end
 
-  
-  
-blablabla
 
-
-
-hgjlfyrfylryulryulry8l
-
-
-
-
-687oi3w57kmw57kmw57kmw57mk,
-
-aSDFPUgefOE978FT 07gfb QEFD
-ASDASDASDASD ASD
-ASDASDD Q2WD 12ED 
-
-
-awdgf24T2 4G2 G4V
+def contain_a(array)
+  element_starting_with_a = []
+  array.each do |word|
+    if word.include?("a") || word.is_a?(String)
+      array.push(word)
+    end
+  end
+  element_starting_with_a
+end
