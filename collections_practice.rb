bd3774e6f8e7da9fbf25ddfb288b7725b70d6777
Christoph Gruener <christophgruener@me.com>
2019-02-04 18:34:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |word| 
+    if word(0) == "r" 
+       return true 
+      elsif word(0) != "r" 
+       return false 
+      end
+    end
+end
+
+  
+  
+blablabla
+
+
+
+hgjlfyrfylryulryulry8l
+
+
+
+
+687oi3w57kmw57kmw57kmw57mk,
+
+aSDFPUgefOE978FT 07gfb QEFD
+ASDASDASDASD ASD
+ASDASDD Q2WD 12ED 
+
+
+awdgf24T2 4G2 G4V
