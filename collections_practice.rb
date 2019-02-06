5a345141ff08f3076e21a260ff81fe6e06f73921
Lu Daley <daleyluis@gmail.com>
2019-02-06 22:06:14 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,7 +31,68 @@ end
 
 def remove_non_strings(array)
 #if .class !== string delete
-#binding.pry
 array.delete_if { |obj| !(obj.is_a? String) }
 #array.grep(String)
 end
+
+def count_elements(arr)
+arr.each_with_object(Hash.new(0)) { |g,h| h[g[:name]] += 1 }.
+    map do |name, count| 
+   { :name=>name, :count=>count } 
+end
+end
+
+def merge_data(keys,data)
+# data[0].values.map.with_index {|v, i| keys[i].merge(v)}
+  data[0].values.map.with_index do |v, i| 
+    keys[i].merge(v)
+  end
+#combines two nested data structures into one (FAILED - 1)
+end
+
+def find_cool(cool)
+ coolington = cool.detect do |n|
+  n[:temperature] == "cool"
+#binding.pry
+ end   
+ [coolington]
+end
+#each << shovel matching values into an array and return new array
+ # cool[1]
+#     if status == "cool"
+#  .map do |label,status|
+#   [:temparature] == "cool"
+
+   #end
+
+=begin
+keys
+=> [{:first_name=>"blake"}, {:first_name=>"ashley"}]
+data
+=> [{"blake"=>{:awesomeness=>10, :height=>"74", :last_name=>"johnson"},
+  "ashley"=>{:awesomeness=>9, :height=>60, :last_name=>"dubs"}}]
+=end
+
+def organize_schools(schools)
+ hash = {}
+ schools.each do |name,info|
+  locations = info[:location]
+    if hash[locations]
+#binding.pry
+    hash[locations]<< name
+else
+  hash[locations] = []
+  hash[locations]<< name
+  end
+ end
+ hash
+end
+#       expected: {"Chicago"=>["dev boot camp chicago"], "NYC"=>["flatiron school bk", "flatiron school", "general assembly"], "SF"=>["dev boot camp", "Hack Reactor"]}
+ #           got: {"Hack Reactor"=>{:location=>"SF"}, "dev boot camp"=>{:location=>"SF"}, "dev boot camp chicago"=>{:lo...:location=>"NYC"}, "flatiron school bk"=>{:location=>"NYC"}, "general assembly"=>{:location=>"NYC"}}
+  
+     #if hashes have the same value==> merge their keys? OR ==>shovel their value into an hash
+     ###if info{=> == "NYC"} shovel into a hash
+     
+      
+#organize_schools organizes the schools by location
+
