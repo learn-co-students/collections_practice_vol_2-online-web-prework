1a12a84c9ecbdf76d04604af156ef9e24537b9ce
Lu Daley <daleyluis@gmail.com>
2019-02-06 04:18:01 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
-# your code goes here
+require 'pry'
+def begins_with_r(words)
+  words.all? {|word| word.start_with?("r")}
+#end
+ #end
+end 
+
+def contain_a(array)
+  array.select { |word| word.include?("a") }
+#review array(name?) return all elements that contain the letter "a".
+end
+
+def first_wa(array)
+ #string = ""
+ #string << array.to_s
+ # binding.pry
+ array.select do |word| 
+  word.to_s.start_with?("wa")
+ end
+end
+#word
+#end
+  #if words.first == "r" 
+  #if words.start_with?("r")
+  #   puts true
+  #else false
+  #end
+ 
+#words.detect{|i| i.start_with?("wa")}
+####words.all? {|word| word.start_with?("wa")}
+#binding.pry
