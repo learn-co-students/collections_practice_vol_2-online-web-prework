d756a8e97bc210e019abe59ede725ae448f2162d
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-02-10 12:35:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -59,31 +59,50 @@ def merge_data(keys, data)
   temp1=[]
   temp2=[]
   temp3=[]
+  arr1 = {}
+  arr2 = {}
+  arr_temp={}
   stemp = ""
-  a_ct=0 
-  b_ct=0 
-  c_ct=0 
-  p "keys length: #{keys.length}"
-  p "data length: #{data.length}" 
-  
-  p data[a_ct].keys[0]
-  p data[a_ct]["blake"]
-  p keys[a_ct][:first_name]
+  stemp2 = ""
+  a_ct=0  
    
-  #while a_ct < keys.length
-    
-    #if keys[a_ct][:first_name] = 
-        #temp1=
-    
-  
-
-  
-  
-  #p keys.merge(data)
-   
+  while a_ct < keys.length
+    stemp = keys[a_ct][:first_name]
+    stemp2 = data[0].keys[a_ct]
+    arr1=keys[a_ct]
+    arr2=data[0][stemp]
+    if stemp == stemp2
+      arr2.merge!(arr1)
+      temp3 << arr2
+    end
+    a_ct += 1
+  end
+  return temp3
 end    
   
+def find_cool(temp)
+  a_ct = 0
+  ans = []
+  while a_ct < temp.length
+    if temp[a_ct][:temperature] == "cool"
+      ans << temp[a_ct]
+    end
+  a_ct += 1 
+  end
+  ans 
+end
+
+def organize_schools(schools)
+  arr1 = []
+  arr2 = []
+  hsh1 = {}
+  hsh2 = {}
+  a_ct=0
+  
+  p hsh1 = schools.group_by{|k, v| v}
+  p hsh1.keys
+  p hsh1.values 
+  
 
 
-
-
+end
