9a88017af9f86dc71ec8018ea60be65c0b7a2c5f
Mark Kanko <mkanko66@gmail.com>
2019-02-12 05:06:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(word_list)
+  word_list.all? {|word| word.start_with?("r")}
+end
+
+#def contain_a(word_list)
+#  word_a_list = []
+#  word_list.each do |word|
+#    if word.include?("a")
+#      word_a_list << word 
+#    end 
+#  end
+#  word_a_list
+#end  
+
+ def contain_a(word_list)
+   word_list.select {|word| word.include?("a")}
+end
+
+def first_wa(word_list)
+  string_array = []
+  word_list.each {|word| string_array << word.to_s}
+  string_array.detect {|word| word.include?("wa")} 
+end 
+
+#def remove_non_strings(word_list)
+#  word_list.delete_if {|word| word != ""}
+#end
+
+def count_elements(array)
+  a_counter = 0 
+  b_counter = 0 
+  array.each do |item|
+    if array[:name] == "ashley"
+      a_counter += 1 
+    elsif 
+      array[:name] == "blake"
+      b_counter += 1 
+      
+      
+    end 
+  end 
+ # binding.pry 
+end
+
+      
+  
+ 
