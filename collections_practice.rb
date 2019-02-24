d45d7fc252a2fb2777ca9e8479fcf673825f3476
Darren L <darren.lou@gmail.com>
2019-02-24 08:16:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,11 +44,48 @@ def count_elements(array)
 
 end
 
-def merge_data
+def merge_data(keys, data)
+  dataIndex = 0
+  data[dataIndex].values.map.with_index {|v, i| keys[i].merge(v)}
 end
 
-def find_cool
+def find_cool(cool)
+
+  cool.select{ |key, hash| key[:temperature] == "cool" }
+
 end
 
-def organize_schools
+def organize_schools(schools)
+#   # schools = {"flatiron school bk"=>{:location=>"NYC"},
+# "flatiron school"=>{:location=>"NYC"},
+# "dev boot camp"=>{:location=>"SF"},
+# "dev boot camp chicago"=>{:location=>"Chicago"},
+# "general assembly"=>{:location=>"NYC"},
+# "Hack Reactor"=>{:location=>"SF"}}
+  
+  # expects {"Chicago"=>["dev boot camp chicago"], "NYC"=>["flatiron school bk", "flatiron school", "general assembly"], "SF"=>["dev boot camp", "Hack Reactor"]}
+  
+  # a hash with keys of the location as strings
+  # each location is assigned an array of school names as strings for that location
+  
+  binding.pry
+  
+  byLocations = {}
+  location_array = []
+  
+  location_array = schools.collect {|name, location| 
+    schools["#{name}"][:location]
+  }.uniq.sort
+  
+  
+  location_array.each { |item|
+    byLocations[item]  = schools.collect {|name, location|
+      if schools[name][:location] == item
+        name
+      end
+    }.compact
+    
+  }
+  
+  byLocations
 end
