140e340e345294be8ce8db29a47e52a7f1dd2bfd
David Thayer <dthayer16@gmail.com>
2019-03-05 22:24:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 # your code goes here
+def begins_with_r(array)
+  if array.include? "r"
+    true
+  else
+    false
+  end
+end
+
+def contain_a(array)
+  if array.include? "a"
+    true
+  else
+    false
+  end
+end
+end
