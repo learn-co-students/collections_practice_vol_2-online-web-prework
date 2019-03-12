2073e9706020dfbccb9fdc790a0c16d5bf7c0874
Nate Tan <nktan93@gmail.com>
2019-03-12 19:50:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,14 +25,68 @@ def remove_non_strings(array)
 end
 
 def count_elements(hash_array)
+  new_array = []
   hash_array.each do |pair|
+    new_hash = {}
     pair_count = hash_array.count(pair)
-    pair[:count] = pair_count
+    pair.each do |key, value|
+      new_hash[key] = value
+    end
+    new_hash[:count] = pair_count
+    new_array << new_hash
   end
-  hash_array.uniq do |pair|
-    pair.keys.first
-  end
+  new_array.uniq
 end
 
 def merge_data(keys, data)
-  
+  new_array = []
+  data.each do |data_hash|
+    data_hash.each do |data_key, data_value|
+      new_hash = {}
+      keys.each do |keys_hash|
+        keys_hash.each do |keys_key, keys_value|
+          if data_key == keys_value
+            #binding.pry
+            new_hash[keys_key] = keys_value
+            data_value.each do |key, value|
+              new_hash[key] = value
+            end
+          end
+        end
+      end
+      new_array << new_hash
+    end
+  end
+  new_array
+end
+
+def find_cool(hash_array)
+  cool_array = []
+  hash_array.each do |hash|
+    hash.each do |key, value|
+      if value == "cool"
+        cool_array << hash
+      end
+    end
+  end
+  cool_array
+end
+
+def organize_schools(schools)
+  new_hash = {}
+  schools.each do |school, hash|
+    hash.each do |location, city|
+      new_hash[city] = []
+    end
+  end
+  schools.each do |school, hash|
+    hash.each do |location, city|
+      new_hash.each do |city2, blank_array|
+        if city == city2
+          new_hash[city] << school
+        end
+      end
+    end
+  end
+  new_hash
+end
