1039311b9f048ce20ce35afd7353ac62cf1209c2
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-25 03:25:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,21 +1,40 @@
+require 'pry'
+
 def begins_with_r(group)
-  group.each do |word|
+  words = [ ]
+  group.map do |word|
    if word.start_with?("r") == true
-    return true
+     words << word
+     if words == group
+       return true
+   else
+     return false
+      end
     end
   end
 end  
 
-def contain_a
-  
+def contain_a(group)
+  words = [ ]
+  group.map do |word|
+    if word.include?("a") == true
+      words << word
+    end  
+  end    
+  return words
 end 
 
-def first wa
+def first_wa
   
 end  
 
-def remove_non_strings
-  
+def remove_non_strings(group)
+  group.map do |item|
+    if item.instance_of?(String) == false
+      word.delete!
+    end
+  end
+  return group
 end  
 
 def count_elements
