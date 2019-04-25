8f1721ed7ec590ce4c1388711495686438c2c370
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-25 03:00:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
-# your code goes here
+def begins_with_r(group)
+  group.each do |word|
+   if word.start_with?("r") == true
+    return true
+    end
+  end
+end  
+
+def contain_a
+  
+end 
+
+def first wa
+  
+end  
+
+def remove_non_strings
+  
+end  
+
+def count_elements
+  
+end  
+
+def merge_data
+  
+end  
+
+def find_cool
+  
+end  
+
+def organize_schools
+  
+end  
