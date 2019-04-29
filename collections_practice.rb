6e7efa2330c92469b2a1981cb14b3b980b25aa57
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-29 00:06:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,16 +47,17 @@ def remove_non_strings(group)
   return strings
 end  
 
-def count_elements(group)
-  group.each do |set|
-    #binding.pry
-      set[:count] = nil
-    set.each do
-      count = set.keys.count
-        set[:count] = count
-    end 
-  end
-end  
+def count_elements(array)
+ array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
 
 def merge_data(a, b)
   
