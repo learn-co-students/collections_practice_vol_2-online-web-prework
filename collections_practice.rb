d4b6c40c3ff0cecae5b88ff45e6fa133081d865f
Angela Lo <angelawenlo@gmail.com>
2019-05-23 20:38:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
 # your code goes here
+require 'pry'
+def begins_with_r(tools)
+    tools.all? do |tool|
+      tool[0] == "r"
+    end
+end
+
+def contain_a(elements)
+  elements.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(words)
+    words.find do |word|
+      word[0..1] == "wa"
+    end
+end
+
+def remove_non_strings(words)
+  words.delete_if do |word|
+    word.class != String
+  end
+end
+
+def count_elements(array)
+  new_hash = {}
+  counter = 0
+  array.each do |info|
+    info[:count] =
+  
+    info.each do |k, v|
+      if array.include?(v)
+        counter +=1
+
+    end
+  end
+end
