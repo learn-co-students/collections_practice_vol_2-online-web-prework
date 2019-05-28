9cf73f05eb3f7c6b424de07595edcf7977710171
Angela Lo <angelawenlo@gmail.com>
2019-05-28 16:52:08 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,15 +25,49 @@ def remove_non_strings(words)
 end
 
 def count_elements(array)
-  new_hash = {}
-  counter = 0
   array.each do |info|
-    info[:count] =
-  
-    info.each do |k, v|
-      if array.include?(v)
-        counter +=1
+    info[:count] = 0
+  array.each do |data|
+    if info[:name] == data[:name]
+      info[:count] += 1
+      end
+    end
+  end.uniq
+end
 
+def merge_data(keys, data)
+    #name.values = individual name
+  data[0].map do |hash, values|
+    new_hash = {}
+    #hash.keys = array of names
+  keys.each do |names|
+    if names[:first_name] == hash
+      new_hash = values.merge(names)
     end
   end
+  new_hash
+end
+end
+
+def find_cool(array)
+  new_array = []
+  array.each do |hash|
+    if hash[:temperature] == "cool"
+      new_array << hash
+    end
+  end
+  new_array
+end
+
+def organize_schools(schools)
+  new_hash = {}
+  schools.each do |program, location|
+  locations = location[:location]
+    if new_hash[locations]
+      new_hash[locations] << program
+    else new_hash[locations] = []
+      new_hash[locations] << program
+  end
+ end
+ new_hash
 end
